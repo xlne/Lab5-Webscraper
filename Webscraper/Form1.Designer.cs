27f8a5fb4b91f8c48526c740a99fde0454ecad0b
Tim Toresson <timtoresson@hotmail.com>
2021-03-21 17:23:23 +0100

    Fixed stuff.

--- Webscraper/Form1.Designer.cs
+++ Webscraper/Form1.Designer.cs
@@ -31,7 +31,7 @@ namespace Webscraper
         {
             this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.saveToFolderButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -39,85 +39,104 @@ namespace Webscraper
             this.button1 = new System.Windows.Forms.Button();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.saveToFolderButton);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.listBox1);
-            this.groupBox1.Location = new System.Drawing.Point(7, 97);
+            this.groupBox1.Location = new System.Drawing.Point(8, 129);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(763, 327);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(862, 436);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Websites";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // button2
+            // saveToFolderButton
             // 
-            this.button2.Location = new System.Drawing.Point(512, 282);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(235, 45);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Save to folder:";
-            this.button2.UseVisualStyleBackColor = true;
+            this.saveToFolderButton.Location = new System.Drawing.Point(585, 376);
+            this.saveToFolderButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.saveToFolderButton.Name = "saveToFolderButton";
+            this.saveToFolderButton.Size = new System.Drawing.Size(269, 60);
+            this.saveToFolderButton.TabIndex = 2;
+            this.saveToFolderButton.Text = "Save to folder:";
+            this.saveToFolderButton.UseVisualStyleBackColor = true;
+            this.saveToFolderButton.Click += new System.EventHandler(this.saveToFolderButton_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(21, 297);
+            this.label2.Location = new System.Drawing.Point(24, 396);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 15);
+            this.label2.Size = new System.Drawing.Size(107, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Images found: ";
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(21, 22);
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(24, 29);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listBox1.Name = "listBox1";
             this.listBox1.ScrollAlwaysVisible = true;
-            this.listBox1.Size = new System.Drawing.Size(726, 244);
+            this.listBox1.Size = new System.Drawing.Size(806, 324);
             this.listBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 39);
+            this.label1.Location = new System.Drawing.Point(32, 52);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 15);
+            this.label1.Size = new System.Drawing.Size(78, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "Insert URL:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(97, 34);
+            this.textBox1.Location = new System.Drawing.Point(111, 45);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(398, 23);
+            this.textBox1.Size = new System.Drawing.Size(454, 27);
             this.textBox1.TabIndex = 4;
             this.toolTip1.SetToolTip(this.textBox1, "Input something here");
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(519, 21);
+            this.button1.Location = new System.Drawing.Point(593, 28);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(235, 51);
+            this.button1.Size = new System.Drawing.Size(269, 68);
             this.button1.TabIndex = 5;
             this.button1.Text = "Extract";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(175, 88);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 6;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(917, 600);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "WebScraper";
             this.groupBox1.ResumeLayout(false);
@@ -136,8 +155,9 @@ namespace Webscraper
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ToolTip toolTip1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button saveToFolderButton;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
